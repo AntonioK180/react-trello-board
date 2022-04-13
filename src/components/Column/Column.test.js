2d91e291c3a6c79c2c1cec501f1868e0badecbd0
AntonioK180 <antonio.kasabov@gmail.com>
2022-04-13 11:42:14 +0300

    Remove unnecessery files.

--- src/components/Column/Column.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Column from './Column';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Column />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
