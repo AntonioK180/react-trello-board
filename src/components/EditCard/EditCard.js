7182417895f975fb38c83380c727d5f5ec86413e
AntonioK180 <antonio.kasabov@gmail.com>
2022-04-17 13:27:55 +0300

    Skeleton of EditCard.

--- src/components/EditCard/EditCard.js
+++ src/components/EditCard/EditCard.js
@@ -3,12 +3,20 @@ import './EditCard.css';
 
 const EditCard = (props) => {
 	let title = 'Title';
+	let description = 'I am describing';
 
 	return (
 		<div className="EditCard">
 			<div className='title-bar'> 
 				<span className='title-bar-icon'></span>
 				{title}
+			</div>
+
+			<div className='card-description'>
+				<span className='description-title-icon'></span>
+				<h3 className='description-title'>Description</h3>
+				
+				<textarea className='description-value' rows="4" cols="80" placeholder='Add a more detailed description…'></textarea>
 			</div>
 
 			<div className='content'> 
