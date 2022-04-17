3527d11a14a6a7c60b8b9715c4efa23ae0a5e76d
AntonioK180 <antonio.kasabov@gmail.com>
2022-04-18 00:50:37 +0300

    Style the add new list button.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -7,6 +7,7 @@ import { v4 as uuidv4 } from 'uuid'
 const Board = (props) => {
     const [columnName, setColumnName] = useState("");
     const [columns, setColumns] = useState([]);
+    const [displayForm, setDisplayForm] = useState(false); 
 
     const getBoard = () => {
         const user = props.users.find(user => user.username === props.loggedUser);
@@ -30,11 +31,15 @@ const Board = (props) => {
         localStorage.setItem("users", JSON.stringify(props.users));
     }
 
+    const toggleAddListForm = () => {
+        setDisplayForm(!displayForm);
+    }
+
     return (
         <div className="board">
             {getBoard().columns.map(column => <Column key={column} name={column.name} order={column.order} />)}
             <div className="add-column">
-                <div>
+                { displayForm ?
                     <form className="add-list">
                         <input type="text"
                             placeholder="Enter list title..."
@@ -42,10 +47,16 @@ const Board = (props) => {
                             onChange={(event) => setColumnName(event.target.value)} />
                         <div className='buttons-wrapper add-list-form'>
                             <button className="save-button cursor-pointer dark-blue-bg" onClick={addColumn}>Add list</button>
-                            <span className='cross-icon cursor-pointer'></span>
+                            <span onClick={toggleAddListForm} className='cross-icon cursor-pointer'></span>
                         </div>
                     </form>
-                </div>
+                : 
+                    <div onClick={toggleAddListForm} className='no-form-wrapper cursor-pointer light-grey-bg'>
+                        <span className='plus-icon-add-list'></span>
+                        <span>Add another list</span>
+                    </div>
+
+                }
             </div>
 
         </div>
