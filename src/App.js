524ca5b291bcca53a43fda094a1bcba2eaa41960
StoyanTinchev <st8tinchev@gmail.com>
2022-04-17 16:54:45 +0300

    Edit: removed app context, and handle the json structure in the code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import './App.css';
 import Login from './components/Login/Login';
 import Homepage from './components/Homepage/Homepage';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 
 const App = () => {
-	(function () {
-		if (localStorage.getItem('users') === null)
-			localStorage.setItem('users', []);
-	})();
-
-	const fetchUser = (users, username) => {
-		for (let i = 0; i < users.length; i++) {
-			if (users[i].username === username)
-				return users[i];
-		}
-
-		return null;
-	}
-
-	const username = localStorage.getItem('loggedUser');
-	if (username !== null){
-		const users = localStorage.getItem('users');
-		const user = fetchUser(users, username);
-		
-		return <Homepage username={username} boards={user.boards}/>
-	} else
-		window.location.href = '/login';
-
-
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
-					{}
+                    <Route path="/*" element={<Login/>} />
+                    {localStorage.getItem('logged_user') ?
+						<>
+							<Route path="/home" element={<Homepage/>} />
+						</>
+                        :
+                        <></>
+                    }
 				</Routes>
 			</BrowserRouter>
 		</>
