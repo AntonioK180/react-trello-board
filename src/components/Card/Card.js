b670dff0f9c73541e59e81ff6044e953128ad547
StoyanTinchev <st8tinchev@gmail.com>
2022-04-25 18:00:37 +0300

    ID: #40 Edit: card movement merge

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,71 +5,77 @@ import EditCard from '../EditCard/EditCard';
 import {BoardService} from "../../services/BoardService";
 
 const Card = (props) => {
-	const boardService = new BoardService();
-	const [displayEdit, setDisplayEdit] = useState(false);
-	const [updateBoard, setUpdateBoard] = useState(true);
+    const boardService = new BoardService();
+    const [displayEdit, setDisplayEdit] = useState(false);
+    const [updateBoard, setUpdateBoard] = useState(true);
 
-	const openEditScreen = () => {
-		setDisplayEdit(true);
-	}
+    const openEditScreen = () => {
+        setDisplayEdit(true);
+    }
 
-	const closeEditScreen = () => {
-		setDisplayEdit(false);
-	}
+    const closeEditScreen = () => {
+        setDisplayEdit(false);
+    }
 
-	const moveCardRigth = () => {
-		const currCard = boardService.getCardByID(props.users,
-			props.loggedUser, props.card_id);
-		const column = boardService.getCardInColumn(props.users,
-			props.loggedUser, props.card_id);
+    const moveCardRigth = () => {
+        const currCard = boardService.getCardByID(props.users,
+            props.loggedUser, props.card_id);
+        const column = boardService.getCardInColumn(props.users,
+            props.loggedUser, props.card_id);
 
-		const nextColumn = boardService.getColumnIDByOrderNumber(props.users,
-			props.loggedUser, column.order + 1);
+        const nextColumn = boardService.getColumnIDByOrderNumber(props.users,
+            props.loggedUser, column.order + 1);
 
-		if (nextColumn) {
-			currCard.column_id = nextColumn.id;
-		}
-		localStorage.setItem("users", JSON.stringify(props.users));
+        if (nextColumn) {
+            currCard.column_id = nextColumn.id;
+        }
+        localStorage.setItem("users", JSON.stringify(props.users));
 
-		setUpdateBoard(!updateBoard);
-	}
+        setUpdateBoard(!updateBoard);
+    }
 
-	const moveCardLeft = () => {
-		const currCard = boardService.getCardByID(props.users,
-			props.loggedUser, props.card_id);
-		const column = boardService.getCardInColumn(props.users,
-			props.loggedUser, props.card_id);
+    const moveCardLeft = () => {
+        const currCard = boardService.getCardByID(props.users,
+            props.loggedUser, props.card_id);
+        const column = boardService.getCardInColumn(props.users,
+            props.loggedUser, props.card_id);
 
-		const prevColumn = boardService.getColumnIDByOrderNumber(props.users,
-			props.loggedUser, column.order - 1);
+        const prevColumn = boardService.getColumnIDByOrderNumber(props.users,
+            props.loggedUser, column.order - 1);
 
-		if (prevColumn) {
-			currCard.column_id = prevColumn.id;
-		}
+        if (prevColumn) {
+            currCard.column_id = prevColumn.id;
+        }
 
-		localStorage.setItem("users", JSON.stringify(props.users));
+        localStorage.setItem("users", JSON.stringify(props.users));
 
-		setUpdateBoard(!updateBoard);
-	}
+        setUpdateBoard(!updateBoard);
+    }
 
-	return (
-		<>
-			<div className="Card">
-				<div className='card-body'>
-					<div onClick={openEditScreen} className='click-to-edit cursor-pointer'> {props.name} </div>
-					<span onClick={moveCardLeft} className='cursor-pointer arrow-left'></span>
-					<span onClick={moveCardRigth} className='cursor-pointer arrow-right'></span>
-				</div>
-			</div>
-			<Portal>
-				{displayEdit ? <EditCard card_id={props.card_id}
-										 column_id={props.column_id}
-										 users={props.users}
-										 loggedUser={props.loggedUser}
-										 closeCallback={closeEditScreen}/> : <></>}
-			</Portal>
-		</>
-	);
+    return (
+        <>
+            <div className="Card">
+                <div className='card-body cursor-pointer'>
+                    <div onClick={openEditScreen} className='click-to-edit cursor-pointer'> {props.name} </div>
+                    <span onClick={moveCardLeft} className='cursor-pointer arrow-left'></span>
+                    <span onClick={moveCardRigth} className='cursor-pointer arrow-right'></span>
+                </div>
+            </div>
+            {props.renderInArchive ?
+                <></> :
+                <Portal>
+                    {displayEdit ? <EditCard card_id={props.card_id}
+                                             column_id={props.column_id}
+                                             users={props.users}
+                                             loggedUser={props.loggedUser}
+                                             closeCallback={closeEditScreen}
+                                             setArchive={props.setArchive}
+											 cardArchived={props.cardArchived}
+                    /> : <></>}
+                </Portal>
+            }
+        </>
+    );
 };
 
 
@@ -78,3 +84,48 @@ Card.propTypes = {};
 Card.defaultProps = {};
 
 export default Card;
+//
+// import React from 'react';
+// import './Card.css';
+// import { useContext, useState } from 'react';
+// import Portal from '../Portal/Portal';
+// import EditCard from '../EditCard/EditCard';
+//
+// const Card = (props) => {
+// 	const [displayEdit, setDisplayEdit] = useState(false);
+//
+// 	const openEditScreen = () => {
+// 		setDisplayEdit(true);
+// 	}
+//
+// 	const closeEditScreen = () => {
+// 		setDisplayEdit(false);
+// 	}
+//
+// 	return (
+// 		<>
+// 			<div className="Card">
+// 				<div className='card-body cursor-pointer' onClick={openEditScreen}> {props.name} </div>
+// 			</div>
+// 			{props.renderInArchive ?
+// 				<></> :
+// 				<Portal>
+// 					{ displayEdit ? <EditCard id={props.id}
+// 											  cardName={props.name}
+// 											  columnName={props.columnName}
+// 											  loggedUser={props.loggedUser}
+// 											  closeCallback = {closeEditScreen}
+// 											  setArchive={props.setArchive}
+// 											  cardArchived={props.cardArchived}/> : <></> }
+// 				</Portal>
+// 			}
+// 		</>
+// 	);
+// };
+//
+//
+// Card.propTypes = {};
+//
+// Card.defaultProps = {};
+//
+// export default Card;
